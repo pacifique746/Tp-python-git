ebefd1c20a2468c56e131c8dbd9c5d2eb88b4355
pacifique746 <pacifique745@gmail.com>
2026-09-23 21:12:28 +0100

    feat:ajout de l'exercice 1 sur le calcul de l'age

---
+++ ex1_age.py
@@ -0,0 +1,5 @@
+nom=input("Quel est votre nom ?")
+annee_naissance= int(input("Quelle est votre annee de naissance ?")) 
+annee_actuelle= 2026
+age = annee_actuelle-annee_naissance
+print(f"Bonjour {nom}, vous avez (ou) aurez {age} ans cette annee")
